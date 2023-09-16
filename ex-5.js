b039445b69d35ec20bcf08f78e8b68df1d767135
Kantima <k.chooarwut@gmail.com>
2023-09-16 23:35:16 +0700

    feat: add assignment from earth

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let sell = 27000;
+let discount =(sell*0.25);
+let pay = (sell-discount);  
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,9 @@
 // Exercise #6: Displaying message
 
 let totalParts = 13000;
-
-let concatSentence;
-let literalSentence;
+let total = "Total: ";
+let parts =" Machine parts";
+let concatSentence = total + totalParts + parts;
+let literalSentence = `Total: ${totalParts} Machine parts`;
+console.log(concatSentence);
+console.log(literalSentence);
